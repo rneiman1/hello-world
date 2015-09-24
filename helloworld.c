a369582e294a277f525b04e7855931818335ec0d
Rachel Neiman <rneiman1@jhu.edu>
2015-09-24 14:18:06 -0400

    Commented stuff

--- helloworld.c
+++ helloworld.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char* argv[]) {
 	printf("Hello world!\n");
-	int i = 0;
-	for(i=0; i < argc; i = i+1) {
-		printf("argv[%d] = %s\n", i, argv[i]);
-}	
+	
+	// int i = 0;
+	// for(i=0; i < argc; i = i+1) {
+	//	printf("argv[%d] = %s\n", i, argv[i]);
+	// }	
 	
 	return EXIT_SUCCESS;
 }
