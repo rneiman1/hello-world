727a513520c6f5de0b2a326a08b791a242b6131a
Rachel Neiman <rneiman1@jhu.edu>
2015-09-24 14:05:18 -0400

    New helloworld program

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello world!\n");
+	int i = 0;
+	for(i=0; i < argc; i = i+1) {
+		printf("argv[%d] = %s\n", i, argv[i]);
+}	
+	
+	return EXIT_SUCCESS;
+}
